ea0334c6fa723ed08c45b26964822461822603d7
Rodrigo Javier <rodrigojaviergd@gmail.com>
2022-06-20 16:59:39 -0400

    commit default

--- FastWing.java
+++ FastWing.java
@@ -21,9 +21,37 @@ import javax.swing.JOptionPane;
 public class FastWing {
 
     public static void main(String[] args) {
-        List<Vuelo> destinos = Arrays.asList() {
-        
-        int seleccion = Integer.parseInt(JOptionPane.showInputDialog(null, "Bienvenido!\n"
-                + "Donde desea ir?"));
+        String[] destinos = {"Brasil", "Argentina", "Peru", "Republica Dominicana", "Colombia"};
+        String mensaje = "";
+        boolean validaValor = true;
+        for (int i = 0; i < destinos.length; i++) {
+            mensaje += (i + 1) + " " + destinos[i] + "\n";
+        }
+
+        String seleccion = JOptionPane.showInputDialog(null, "Bienvenido!\n"
+                + "Donde desea ir? \n"
+                + mensaje);
+        int seleccionNum = 0;
+
+        if (seleccion.equals("1") || seleccion.equals("2") || seleccion.equals("3") || seleccion.equals("4") || seleccion.equals("5")) {
+            seleccionNum = Integer.parseInt(seleccion);
+            validaValor = false;
+        } else {
+            while (validaValor) {
+                if (seleccionNum >= 1 && seleccionNum <= 5) {
+                    JOptionPane.showMessageDialog(null, "Por favor, selccione un destino valido");
+                    seleccion = JOptionPane.showInputDialog(null, "Bienvenido!\n"
+                            + "Donde desea ir? \n"
+                            + mensaje);
+                    if (seleccion.equals("1") || seleccion.equals("2") || seleccion.equals("3") || seleccion.equals("4") || seleccion.equals("5")) {
+                        seleccionNum = Integer.parseInt(seleccion);
+                        validaValor = false;
+                    }
+                } else {
+                    validaValor = false;
+                }
+            }
+        }
+
     }
 }
